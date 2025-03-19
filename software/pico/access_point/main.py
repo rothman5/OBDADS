c32eaf86dc676292cb3ca9d9d55904bb0a9d7777
rothman5 <rothman@ualberta.ca>
2025-03-18 18:39:40 -0600

    moved pico project to software folder

--- software/pico/access_point/main.py
+++ software/pico/access_point/main.py
@@ -1,5 +1,11 @@
-import network
 import time
+
+import machine
+import network
+
+# Define the LED pin
+led = machine.Pin('LED', machine.Pin.OUT)
+
 
 # Configure AP
 ap = network.WLAN(network.AP_IF)
@@ -11,3 +17,8 @@ while not ap.active():
     time.sleep(1)
 
 print("AP Mode Active. IP Address:", ap.ifconfig()[0])
+
+# Flash the LED every second
+while True:
+    led.value(not led.value())
+    time.sleep(1)
