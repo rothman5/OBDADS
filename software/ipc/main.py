9b659215f11808543736e51f5d52c2787404fa75
rothman5 <rothman@ualberta.ca>
2025-03-24 10:57:06 -0600

    IPC writes to csv

--- software/ipc/main.py
+++ software/ipc/main.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import time
+from pathlib import Path
 
 import serial
 
@@ -8,10 +9,28 @@ IPC_ID = "OBDADS-IPC"
 IPC_PORT = "/dev/ttyRPMSG0"
 DELAY_MS = 500
 BATCH_SIZE = 100
-REMOTE_PROC_PATH = "/sys/class/remoteproc/remoteproc0/state"
-CONTROL_COMMAND_START = [f"echo start > {REMOTE_PROC_PATH}"]
-CONTROL_COMMAND_STOP = [f"echo stop > {REMOTE_PROC_PATH}"]
-CONTROL_COMMAND_STATE = ["cat", REMOTE_PROC_PATH]
+REMOTEPROC_DIR = "/sys/class/remoteproc/remoteproc0"
+CONTROL_COMMAND_START = [f"echo start > {REMOTEPROC_DIR}/state"]
+CONTROL_COMMAND_STOP = [f"echo stop > {REMOTEPROC_DIR}/state"]
+CONTROL_COMMAND_STATE = ["cat", f"{REMOTEPROC_DIR}/state"]
+COLUMNS = [
+    "Ax (g)",
+    "Ay (g)",
+    "Az (g)",
+    "Gx (dps)",
+    "Gy (dps)",
+    "Gz (dps)",
+    "Temp (degC)",
+    "engine_speed (rpm)",
+    "vehicle_speed (km/h)",
+    "engine_load (%)",
+    "throttle_position (%)",
+    "distance_traveled_mil (km)",
+    "o2_volt1 (V)",
+    "o2_volt2 (V)",
+    "o2_curr1 (A)",
+    "o2_curr2 (A)",
+]
 
 
 csv_buffer: list[list[str]] = []
@@ -21,18 +40,23 @@ def fw_state() -> str:
     return subprocess.check_output(CONTROL_COMMAND_STATE).decode("utf-8").strip()
 
 
-def stop_firmware() -> bool:
+def load_firmware(fw: Path) -> None:
+    subprocess.run([f"echo {fw} > {REMOTEPROC_DIR}/firmware"], shell=True, check=True)
+    time.sleep(DELAY_MS / 1000)
+
+
+def stop_firmware(script: Path) -> bool:
     if fw_state() == "offline":
         return True
 
-    subprocess.run(CONTROL_COMMAND_STOP, shell=True, check=True)
+    subprocess.run([f"{script} stop"], shell=True, check=True)
     time.sleep(DELAY_MS / 1000)
 
     return True if fw_state() == "offline" else False
 
 
-def start_firmware() -> bool:
-    subprocess.run(CONTROL_COMMAND_START, shell=True, check=True)
+def start_firmware(script: Path) -> bool:
+    subprocess.run([f"{script} start"], shell=True, check=True)
     time.sleep(DELAY_MS / 1000)
 
     return True if fw_state() == "running" else False
@@ -66,7 +90,16 @@ def process_line(line: str) -> None:
     if len(csv_buffer) >= BATCH_SIZE:
         print(f"Batch size: {len(csv_buffer)}, each batch {len(csv_buffer[0])} columns")
         # TODO: Add to database here
+        exists = Path("tmp.csv").exists()
+        with open(file="tmp.csv", mode="a", encoding="utf-8") as csv:
+            if not exists:
+                csv.write(", ".join(COLUMNS) + "\n")
+            else:
+                for row in csv_buffer:
+                    csv.write(", ".join(row) + "\n")
         csv_buffer.clear()
+    else:
+        print(f"Batched [{len(csv_buffer)} / {BATCH_SIZE}]")
 
 
 def listen_ipc() -> None:
@@ -92,11 +125,23 @@ def listen_ipc() -> None:
 
 
 def main() -> None:
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <firmware>")
+        sys.exit(1)
+
+    firmware = Path(sys.argv[1]).resolve()
+    if not firmware.exists():
+        print(f"Error: Firmware file {firmware} does not exist!")
+        sys.exit(1)
+
+    script = firmware.parent.parent.parent / "fw_cortex_m4.sh"
+
     try:
-        if not stop_firmware():
+        load_firmware(firmware)
+        if not stop_firmware(script):
             print("Failed to stop firmware!")
             sys.exit(1)
-        if not start_firmware():
+        if not start_firmware(script):
             print("Failed to start firmware!")
             sys.exit(1)
         if not initiate_ipc():
@@ -107,7 +152,7 @@ def main() -> None:
         print("Keyboard interrupt detected!")
     finally:
         print("Stopping firmware...")
-        stop_firmware()
+        stop_firmware(script)
         sys.exit(1)
 
 
