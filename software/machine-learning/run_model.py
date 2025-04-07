8396d9e26eabe9d851107a7af0b7c595bc820cc6
Matteo <matteof@telus.net>
2025-04-07 15:25:14 -0600

    Updated ML code to access database and repeat running every 10/1 minutes

--- software/machine-learning/run_model.py
+++ software/machine-learning/run_model.py
@@ -2,88 +2,106 @@
 Run the machine learning model for the OBDADS project.
 """
 
-import pandas as pd
-import numpy as np
 import sqlite3
-import matplotlib.pyplot as plt
+import time
 
-import tensorflow as tf
+import numpy as np
+import pandas as pd
 from keras._tf_keras import keras
-from keras._tf_keras.keras.models import Sequential
-from keras._tf_keras.keras.layers import LSTM, Dense, RepeatVector, TimeDistributed
 from sklearn.preprocessing import MinMaxScaler
 
-from common import features, SEQ_LENGTH, THRESHOLD, create_sequences, samples_in_10_minutes
+from common import SEQ_LENGTH, THRESHOLD, create_sequences, features, samples_in_10_minutes
 
-# Load the model
-model = keras.models.load_model('model.keras')
-print('Model loaded from model.keras')
 
-# Make a copy of the model
-model_copy = keras.models.clone_model(model)
-model_copy.set_weights(model.get_weights())
-print('Model copy created')
+def run_model():
+    try:
+        # Load the model
+        model = keras.models.load_model('model.keras')
+        print('Model loaded from model.keras')
 
-# Connect to DB
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
+        # Make a copy of the model
+        model_copy = keras.models.clone_model(model)
+        model_copy.set_weights(model.get_weights())
+        print('Model copy created')
 
-# Read the last minute of data from the data table
-cursor.execute('SELECT * FROM data ORDER BY timestamp DESC LIMIT ?', (samples_in_10_minutes,))
+        # Connect to DB
+        conn = sqlite3.connect('tmp.db')
+        cursor = conn.cursor()
 
-# Select the relevant columns
-rows = cursor.fetchall()
-data = pd.DataFrame(rows, columns=['timestamp'] + features)
-data = data[features].values
-# Close the cursor and connection
-cursor.close()
-conn.close()
+        # Read the last minute of data from the data table
+        cursor.execute('SELECT engine_speed,vehicle_speed,throttle_pos,engine_load FROM OBD_DATA ORDER BY time DESC LIMIT ?', (samples_in_10_minutes,))
 
-# Normalize the data
-scaler = MinMaxScaler()
-data = scaler.fit_transform(data)
+        # Select the relevant columns
+        rows = cursor.fetchall()
+        data = pd.DataFrame(rows, columns=features)
+        data = data[features].values
 
-# Create sequences
-data_last_minute = create_sequences(data, SEQ_LENGTH)
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
 
-# Detect anomalies
-reconstructed_data = model_copy.predict(data_last_minute)
-reconstruction_error = np.mean(np.abs(reconstructed_data - data_last_minute), axis=-1)
+        # Normalize the data
+        scaler = MinMaxScaler()
+        data = scaler.fit_transform(data)
 
-# Get the reconstruction error for each feature
-avg_reconstruction_error = np.mean(reconstruction_error, axis=0)
+        # Create sequences
+        data_last_minute = create_sequences(data, SEQ_LENGTH)
 
-try:
-    conn = sqlite3.connect('test.db')
-    cursor = conn.cursor()
-    print("Connected to SQLite")
-except sqlite3.Error as e:
-    print(f"Error connecting to SQLite: {e}")
-    
-# Create a table for the data
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS ml (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        start_time TEXT NOT NULL,
-        description TEXT NOT NULL
-    )
-''')
+        # Detect anomalies
+        reconstructed_data = model_copy.predict(data_last_minute)
+        reconstruction_error = np.mean(np.abs(reconstructed_data - data_last_minute), axis=-1)
 
-# Print whether there is an anomaly for each stat
-for i, feature in enumerate(features):
-    if avg_reconstruction_error[i] > THRESHOLD:
-        print(f'Anomaly detected in {feature} with error {avg_reconstruction_error[i]}')
-        
-        # Construct the message
-        description = f'Anomaly detected in {feature} with error {avg_reconstruction_error[i]}'
-        
-        # Insert the data into the database
+        # Get the reconstruction error for each feature
+        avg_reconstruction_error = np.mean(reconstruction_error, axis=0)
+
+        try:
+            conn = sqlite3.connect('test.db')
+            cursor = conn.cursor()
+            print("Connected to SQLite")
+        except sqlite3.Error as e:
+            print(f"Error connecting to SQLite: {e}")
+
+        # Create a table for the data
         cursor.execute('''
-            INSERT INTO ml (start_time, description)
-            VALUES (datetime("now"), ?)
-        ''', (description,))
-        
-        conn.commit()
-        print(f'Inserted anomaly into database: {description}')
-    else:
-        print(f'No anomaly detected in {feature} with error {avg_reconstruction_error[i]}')
+            CREATE TABLE IF NOT EXISTS ML_DATA (
+                aid INTEGER PRIMARY KEY AUTOINCREMENT,
+                time TEXT NOT NULL,
+                desc TEXT NOT NULL
+            )
+        ''')
+
+        # Print whether there is an anomaly for each stat
+        for i, feature in enumerate(features):
+            if avg_reconstruction_error[i] > THRESHOLD:
+                print(f'Anomaly detected in {feature} with error {avg_reconstruction_error[i]}')
+
+                # Construct the message
+                description = f'Anomaly detected in {feature} with error {avg_reconstruction_error[i]}'
+
+                # Insert the data into the database
+                cursor.execute('''
+                    INSERT INTO ML_DATA (time, desc)
+                    VALUES (datetime("now"), ?)
+                ''', (description,))
+
+                conn.commit()
+                print(f'Inserted anomaly into database: {description}')
+            else:
+                print(f'No anomaly detected in {feature} with error {avg_reconstruction_error[i]}')
+    except sqlite3.Error as e:
+        print(f"Error connecting to SQLite: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if conn:
+            conn.close()
+        print("SQLite connection closed")
+        print("Model run completed")
+
+
+if __name__ == "__main__":
+    # Run the model every 10 seconds
+    while True:
+        run_model()
+        time.sleep(10)
+        # Sleep for 10 seconds
--- software/machine-learning/train_model.py
+++ software/machine-learning/train_model.py
@@ -2,59 +2,76 @@
 Code to train a model for the anomaly detection task using a recurrent neural network (RNN) with LSTM layers.
 """
 
-import pandas as pd
-import numpy as np
 import sqlite3
-import matplotlib.pyplot as plt
+import time
 
-import tensorflow as tf
+import pandas as pd
 from keras._tf_keras import keras
-from keras._tf_keras.keras.models import Sequential
 from keras._tf_keras.keras.layers import LSTM, Dense, RepeatVector, TimeDistributed
+from keras._tf_keras.keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
 
-from common import features, SEQ_LENGTH, THRESHOLD, create_sequences, samples_in_10_minutes
+from common import SEQ_LENGTH, create_sequences, features, samples_in_10_minutes
 
-# Define the model
-model = Sequential([
-    LSTM(64, activation='relu', input_shape=(SEQ_LENGTH, len(features)), return_sequences=True),
-    LSTM(32, activation='relu', return_sequences=False),
-    RepeatVector(SEQ_LENGTH),
-    LSTM(32, activation='relu', return_sequences=True),
-    LSTM(64, activation='relu', return_sequences=True),
-    TimeDistributed(Dense(len(features)))
-])
 
-# Compile the model
-model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+def train_model():
+    try:
+        # Define the model
+        model = Sequential([
+            LSTM(64, activation='relu', input_shape=(SEQ_LENGTH, len(features)), return_sequences=True),
+            LSTM(32, activation='relu', return_sequences=False),
+            RepeatVector(SEQ_LENGTH),
+            LSTM(32, activation='relu', return_sequences=True),
+            LSTM(64, activation='relu', return_sequences=True),
+            TimeDistributed(Dense(len(features)))
+        ])
 
-# Read the last 10 minutes of data from sqlite
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
+        # Compile the model
+        model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
-# Read the last 10 minutes of data from the data table
-cursor.execute('SELECT * FROM data ORDER BY timestamp DESC LIMIT ?', (samples_in_10_minutes,))
+        # Read the last 10 minutes of data from sqlite
+        conn = sqlite3.connect('tmp.db')
+        cursor = conn.cursor()
 
-# Select the relevant columns
-rows = cursor.fetchall()
-data = pd.DataFrame(rows, columns=['timestamp'] + features)
-data = data[features].values
+        # Read the last 10 minutes of data from the data table
+        cursor.execute('SELECT engine_speed,vehicle_speed,throttle_pos,engine_load FROM OBD_DATA ORDER BY time DESC LIMIT ?', (samples_in_10_minutes,))
 
-# Close the cursor and connection
-cursor.close()
-conn.close()
+        # Select the relevant columns
+        rows = cursor.fetchall()
+        data = pd.DataFrame(rows, columns=features)
+        data = data[features].values
 
-# Normalize the data
-scaler = MinMaxScaler()
-data = scaler.fit_transform(data)
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
 
-# Create sequences
-X_train = create_sequences(data, SEQ_LENGTH)
+        # Normalize the data
+        scaler = MinMaxScaler()
+        data = scaler.fit_transform(data)
 
-# Train the model
-early_stopping = keras.callbacks.EarlyStopping(patience=2, restore_best_weights=True)
-model.fit(X_train, X_train, epochs=100, batch_size=64, validation_split=0.1, callbacks=[early_stopping])
+        # Create sequences
+        X_train = create_sequences(data, SEQ_LENGTH)
 
-# Save the model
-model.save('model.keras')
-print('Model saved to model.keras')
+        # Train the model
+        early_stopping = keras.callbacks.EarlyStopping(patience=2, restore_best_weights=True)
+        model.fit(X_train, X_train, epochs=100, batch_size=64, validation_split=0.1, callbacks=[early_stopping])
+
+        # Save the model
+        model.save('model.keras')
+        print('Model saved to model.keras')
+    except sqlite3.Error as e:
+        print(f"Error connecting to SQLite: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if conn:
+            conn.close()
+        print("SQLite connection closed")
+        print("Model training completed")
+
+
+if __name__ == "__main__":
+    # Run a training session every 10 minutes
+    while True:
+        train_model()
+        time.sleep(600)  # Sleep for 10 minutes
